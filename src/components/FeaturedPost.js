d4ba5e59ca9f3d06fc4fd230b93ff9c2965948c8
trivekram-s <s.trivekram@genpact.com>
2026-06-17 15:40:37 +0530

    Update FeaturedPost.js

--- src/components/FeaturedPost.js
+++ src/components/FeaturedPost.js
@@ -1,1 +1,18 @@
-#FeaturedPost.js
+import React from "react";
+
+function FeaturedPost({ post }) {
+  return (
+    <section className="featured-post">
+      <h2>Featured Post</h2>
+      <div className="featured-card">
+        <img src={post.image} alt={post.title} />
+        <h3>{post.title}</h3>
+        <p className="date">{post.date}</p>
+        <p>{post.summary}</p>
+        <button>Read More</button>
+      </div>
+    </section>
+  );
+}
+
+export default FeaturedPost;
