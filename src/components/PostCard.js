194a8559dc6345c5936886fdea260942abc6cd1a
trivekram-s <s.trivekram@genpact.com>
2026-06-17 15:41:15 +0530

    Update PostCard.js

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,1 +1,15 @@
-#PostCard.js
+import React from "react";
+
+function PostCard({ post }) {
+  return (
+    <div className="post-card">
+      <img src={post.image} alt={post.title} />
+      <h3>{post.title}</h3>
+      <p className="date">{post.date}</p>
+      <p>{post.summary}</p>
+      <button>Read More</button>
+    </div>
+  );
+}
+
+export default PostCard;
