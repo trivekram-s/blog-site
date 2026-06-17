c2e0e1835f4f22b6e736c28d5e4e1fe6bcdf20e8
trivekram-s <s.trivekram@genpact.com>
2026-06-17 15:40:56 +0530

    Update PostList.js

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,1 +1,17 @@
-#PostList.js
+import React from "react";
+import PostCard from "./PostCard";
+
+function PostList({ posts }) {
+  return (
+    <section className="post-list">
+      <h2>Recent Posts</h2>
+      <div className="posts-grid">
+        {posts.map((post) => (
+          <PostCard key={post.id} post={post} />
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default PostList;
