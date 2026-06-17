7df2c297da89dc00e7262e806f7a622a4deb3ff6
trivekram-s <s.trivekram@genpact.com>
2026-06-17 15:39:33 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,14 @@
-#Header.js
+import React from "react";
+import NavBar from "./NavBar";
+
+function Header({ title, author }) {
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <p>by {author}</p>
+      <NavBar />
+    </header>
+  );
+}
+
+export default Header;
