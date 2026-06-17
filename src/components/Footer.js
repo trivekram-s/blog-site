3d059ea130d388eee9a39fd896454ee6cb2d4a21
trivekram-s <s.trivekram@genpact.com>
2026-06-17 15:41:40 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,11 @@
-#Footer.js
+import React from "react";
+
+function Footer({ author }) {
+  return (
+    <footer className="footer">
+      <p>&copy; 2025 {author}. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
