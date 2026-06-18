495759d0e417cb4ed19f28af6791b9a290b13ed0
trivekram-s <s.trivekram@genpact.com>
2026-06-18 16:18:35 +0530

    rm

--- blog-site/vitest.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-  test: {
-    include: ['src/**/*.{test,spec}.{js,jsx}'],
-    reporters: 'junit',
-    outputFile: './junit.xml'
-  }
-});
--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-const App = () => {
-    return (
-        <div>
-            <h1>Welcome to the Blog Site</h1>
-        </div>
-    );
-};
-
-export default App;
